4b532e62f312228965af700d8c7fe62827225db2
deptage <34195923+Deptage@users.noreply.github.com>
2022-12-17 16:15:05 +0100

    Add files via upload

---
+++ project-1/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("MatrixAlmostExpert - a program to perform various operations on matrices by Zuzanna Ławniczak, PUT student.");
+        Menu userMemory = new Menu();
+        //Import example matrices to the memory
+        Example ex1 = new Example();
+        userMemory.insertAMatrix(ex1.firstExampleMatrix());
+        userMemory.insertAMatrix(ex1.secondExampleMatrix());
+        userMemory.showMenu();
+    }
+}
---
+++ project-1/Operations.java
@@ -0,0 +1,10 @@
+public interface Operations {
+    //this interface is implemented by the Matrix class
+    //addition, subtraction are methods
+    //multiplication, transposition are handled by constructors in Matrix class
+    void show();
+
+    Operations addToThisMatrix(Matrix m1);
+
+    Operations subtractFromThisMatrix(Matrix m2);
+}
