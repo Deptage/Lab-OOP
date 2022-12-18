127c0962ef8f74bdae39e3289ffa050d5bb8cd1b
deptage <34195923+Deptage@users.noreply.github.com>
2022-12-18 16:17:48 +0100

    Delete Project directory

--- Project/Operations.java
+++
@@ -1,9 +0,0 @@
-public interface Operations {
-    //addition, substraction, multiplications, looking for an inverse matrix
-    Operations addition(double add);
-    Operations substraction(double sub);
-    Operations multiplication(double mul);
-    Operations division(double div);
-    double pvalue();
-
-}
